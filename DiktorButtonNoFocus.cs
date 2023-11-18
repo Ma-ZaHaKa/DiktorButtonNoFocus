dd7c250ec355974cefea4a4d4dabfd118a9fd868
Валодя Петров <96941376+Ma-ZaHaKa@users.noreply.github.com>
2023-11-18 21:18:45 +0200

    Add files via upload

---
+++ DiktorButtonNoFocus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomButton
+{
+    class DiktorButtonNoFocus : Button // из управления close minimize 
+    {
+        public DiktorButtonNoFocus()
+            : base()
+        {
+            this.SetStyle(ControlStyles.Selectable, false);
+        }
+    }
+
+}
